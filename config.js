041a3794d1edd6d5088f40e0b835b3e7891aaa63
ssqswswqsdqwsqwdsqw <66263919+candravima@users.noreply.github.com>
2022-03-09 20:38:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -89,7 +89,7 @@ module.exports = {
   ANTIJID: process.env.ANTIJID === undefined ? "" : process.env.ANTIJID,
   STICKER_PACKNAME:
     process.env.STICKER_PACKNAME === undefined
-      ? "🥰,lyfe00011"
+      ? "🥰,MozACandd"
       : process.env.STICKER_PACKNAME,
   BRAINSHOP:
     process.env.BRAINSHOP === undefined
